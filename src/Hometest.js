59cbbc9b62ac749b352f2c68c5457fad7c2fb493
julian.shomali <julian.shomali@detmrm-nmx60227.na.corp.ipgnetwork.com>
2019-07-25 16:01:14 -0400

    initial commit

--- src/Hometest.js
+++ src/Hometest.js
@@ -18,9 +18,11 @@ class Hometest extends Component {
           You’ve been through so much, and it’s only the beginning. Graduating from college simply means starting the rest of your life, so be prepared. Whether you journey to a job, internship, master’s program or to another state just for the travel, make sure you enjoy just that: the journey. Don’t just get there, Drive There. Apply today to get your discount code for your new GM car.
         </div>
         <Slides/>
-        <div className="parallax-two"></div>
-        <div className="main-text-two">
-          We get it. Somethings can be a total mystery. Credit scores are impossible to understand, what car to buy can be impossible to decide, but these things don’t have to stay a mystery. Here, you can learn everything you need to know to start your life after college. Search your question now for certified answers on almost every topic of life beyond school.
+        <div class="grid2x2">
+          <div class="box box1"><div></div></div>
+          <div class="box box2"><div>We get it. Somethings can be a total mystery. Credit scores are impossible to understand, what car to buy can be impossible to decide, but these things don’t have to stay a mystery. Here, you can learn everything you need to know to start your life after college. Search your question now for certified answers on almost every topic of life beyond school.</div></div>
+          <div class="box box3"><div>We get it. Somethings can be a total mystery. Credit scores are impossible to understand, what car to buy can be impossible to decide, but these things don’t have to stay a mystery. Here, you can learn everything you need to know to start your life after college. Search your question now for certified answers on almost every topic of life beyond school.</div></div>
+          <div class="box box4"><div></div></div>
         </div>
       </div>
     );
@@ -28,3 +30,14 @@ class Hometest extends Component {
 }
 
 export default Hometest;
+
+// <div className="head-parallax">
+//   <div className="parallax-two"></div>
+//   <div className="parallax-three"></div>
+// </div>
+// <div className="main-text-two">
+//   Somethings can be a total mystery. Credit scores are impossible to understand, what car to buy can be impossible to decide, but these things don’t have to stay a mystery. Here, you can learn everything you need to know to start your life after college. Search your question now for certified answers on almost every topic of life beyond school.
+// </div>
+// <div className="main-text-three">
+//   We get it. Somethings can be a total mystery. Credit scores are impossible to understand, what car to buy can be impossible to decide, but these things don’t have to stay a mystery. Here, you can learn everything you need to know to start your life after college. Search your question now for certified answers on almost every topic of life beyond school.
+// </div>
