18ce06a8a87bc7d673f059b15cbc6c484256f25b
julian.shomali <julian.shomali@detmrm-nmx60227.na.corp.ipgnetwork.com>
2019-07-25 16:03:34 -0400

    Updates

---
+++ precache-manifest.21b7e1d5fbcfd8b6c44f5e0e8da83ec2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "986357fb23ed64f2e7e3c2f5a6ce025b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f5ff1744db61758769bb",
+    "url": "/my-app/static/css/main.06438096.chunk.css"
+  },
+  {
+    "revision": "61f1206b32a49e3c63bc",
+    "url": "/my-app/static/js/2.32bb4ff9.chunk.js"
+  },
+  {
+    "revision": "f5ff1744db61758769bb",
+    "url": "/my-app/static/js/main.5b1e24a5.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "fcd2914630e78374250ee62534607839",
+    "url": "/my-app/static/media/book.fcd29146.png"
+  },
+  {
+    "revision": "33a11b22944a139179bff593f2b3da33",
+    "url": "/my-app/static/media/concert.33a11b22.jpg"
+  },
+  {
+    "revision": "3784cf405334cafb98c6f6d18bd43390",
+    "url": "/my-app/static/media/graduationpic.3784cf40.jpg"
+  },
+  {
+    "revision": "df46793e0f632a58abf8d97484c20ae8",
+    "url": "/my-app/static/media/lyft2.df46793e.jpg"
+  },
+  {
+    "revision": "6f1ccb6d3fa433ced6e9fc982ae818e6",
+    "url": "/my-app/static/media/whats_next.6f1ccb6d.svg"
+  }
+]);
