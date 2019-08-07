fc11061e5ffe5b1c211ff90d783883b3f57f3ce6
julian.shomali <julian.shomali@detmrm-nmx60227.na.corp.ipgnetwork.com>
2019-08-07 15:55:17 -0400

    Updates

---
+++ precache-manifest.6176ae30016044fe87ba6caa657cc71e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17fa3dc7964b242d289fcdd2f1231df",
+    "url": "/mrm-mccann-gm-vpp/index.html"
+  },
+  {
+    "revision": "42cecfb46b10c2295bf1",
+    "url": "/mrm-mccann-gm-vpp/static/css/main.d1ff27aa.chunk.css"
+  },
+  {
+    "revision": "61f1206b32a49e3c63bc",
+    "url": "/mrm-mccann-gm-vpp/static/js/2.32bb4ff9.chunk.js"
+  },
+  {
+    "revision": "42cecfb46b10c2295bf1",
+    "url": "/mrm-mccann-gm-vpp/static/js/main.4dc4581f.chunk.js"
+  },
+  {
+    "revision": "17ffb4dcf021d560e868",
+    "url": "/mrm-mccann-gm-vpp/static/js/runtime~main.7ea1a10d.js"
+  },
+  {
+    "revision": "9c35bf87f23c8cca614720126fe0baa0",
+    "url": "/mrm-mccann-gm-vpp/static/media/Gotham-Bold.9c35bf87.otf"
+  },
+  {
+    "revision": "4ac7dbb4f9f9f467db596e759b8bf7db",
+    "url": "/mrm-mccann-gm-vpp/static/media/Gotham-Thin.4ac7dbb4.otf"
+  },
+  {
+    "revision": "fcd2914630e78374250ee62534607839",
+    "url": "/mrm-mccann-gm-vpp/static/media/book.fcd29146.png"
+  },
+  {
+    "revision": "33a11b22944a139179bff593f2b3da33",
+    "url": "/mrm-mccann-gm-vpp/static/media/concert.33a11b22.jpg"
+  },
+  {
+    "revision": "3784cf405334cafb98c6f6d18bd43390",
+    "url": "/mrm-mccann-gm-vpp/static/media/graduationpic.3784cf40.jpg"
+  },
+  {
+    "revision": "df46793e0f632a58abf8d97484c20ae8",
+    "url": "/mrm-mccann-gm-vpp/static/media/lyft2.df46793e.jpg"
+  },
+  {
+    "revision": "891cfa8f3b98a198403ee2cb33616ec1",
+    "url": "/mrm-mccann-gm-vpp/static/media/throwcap.891cfa8f.jpg"
+  },
+  {
+    "revision": "6f1ccb6d3fa433ced6e9fc982ae818e6",
+    "url": "/mrm-mccann-gm-vpp/static/media/whats_next.6f1ccb6d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.1279ef610046513617f1e8449ccdd734.js"
+  "/mrm-mccann-gm-vpp/precache-manifest.6176ae30016044fe87ba6caa657cc71e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mrm-mccann-gm-vpp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
